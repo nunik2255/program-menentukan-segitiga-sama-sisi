8fd9a063acdd1b1461a490cef8d6521bbc34251b
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 17:08:13 +0700

    Add files via upload

---
+++ lat7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+int a,b,c;
+cout<<"masukan sisi a"<<endl;
+cin>>a;
+cout<<"masukan sisi b"<<endl;
+cin>>b;
+cout<<"masukan sisi c"<<endl;
+cin>>c;
+if (a=b=c) {
+    cout<<"segitiga sama sisi"<<endl;
+}
+return 0;
+}
